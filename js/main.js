8d17a8cc71da52ad783630ba0a6a0991a3da16c8
bwlee <lbw3973@naver.com>
2023-01-09 21:47:24 +0900

    feat: Add feature
    1. 검색 기능
    2. 무한 스크롤 기능
    3. to-top 버튼 추가

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 const inputEl = document.querySelector('input')
 const buttonEl = document.querySelector('button#btnSearch')
 const moviesEl = document.querySelector('ul.movies')
-const viewMoreEl = document.querySelector('.view-more')
+const counterEl = document.querySelector('.counter')
+const toTopEl = document.querySelector('#to-top');
 
 let searchText = ''
+let pageNumber = 1
 
-inputEl.addEventListener('input', function () {
-  searchText = inputEl.value
-})
 inputEl.addEventListener("keyup", function (event) {
   if(event.key === 'Enter'){
     event.preventDefault()
@@ -16,23 +15,58 @@ inputEl.addEventListener("keyup", function (event) {
 })
 
 buttonEl.addEventListener('click', async function () {
+  searchText = inputEl.value
+  if(searchText.length < 3){
+    alert("Please enter at least 3 characters")
+    return
+  }
+  pageNumber = 1
   moviesEl.innerHTML = ''
-  const movies = await getMovies()
+
+  const movies = await getMovies(searchText,'', pageNumber)
+  await getCountMovies(searchText)
+
   if(movies != null){
     renderMovies(movies, true)
   }
 })
 
-viewMoreEl.addEventListener('click', async function() {
-  const movies = await getMovies()
+const getCountMovies = async (title, year = '') => {
+  if(!document.querySelector('.movieCounter')){
+    let divEl = document.createElement('div')
+    divEl.classList.add('movieCounter')
+    document.querySelector('section .counter').append(divEl)
+  }
+
+  const countEl = document.querySelector('.movieCounter')
+  const s = `&s=${title}`
+  const y = `&y=${year}`
+
+  const res = await fetch(`https://omdbapi.com/?apikey=7035c60c&s=${s}${y}`)
+  const json = await res.json()
+
+  if(json.Response === 'True'){
+    countEl.innerHTML = `There are ${json.totalResults} movies searched`
+  }else{
+    countEl.innerHTML = 'Movie Not Found!!'
+  }
+
+  counterEl.append(countEl)
+}
+
+const viewmore = async () => {
+  const movies = await getMovies(searchText, '', ++pageNumber)
   if(movies != null){
     renderMovies(movies, false)
   }
-})
+}
 
 let moviess = []
-async function getMovies() {
-  const res = await fetch(`https://omdbapi.com/?apikey=7035c60c&s=${searchText}`)
+async function getMovies(title, year = '', page = 1) {
+  const s = `&s=${title}`
+  const y = `&y=${year}`
+  const p = `&page=${page}`
+  const res = await fetch(`https://omdbapi.com/?apikey=7035c60c&s=${s}${y}${p}`)
   const json = await res.json()
   console.log(json)
   moviess = json.Search
@@ -46,15 +80,59 @@ async function getMovies() {
 function renderMovies(movies, isFirst) {
   const movieEls = movies.map(function (movie) {
     const liEl = document.createElement('li')
-    const titleEl = document.createElement('h2')
-    const posterEl = document.createElement('img')
+    const posterEl = document.createElement('a')
+    const imgEl = document.createElement('div')
+    const infoEl = document.createElement('div')
+    const titleEl = document.createElement('span')
+    const yearEl = document.createElement('span')
 
+    infoEl.append(titleEl)
+    infoEl.append(yearEl)
+    posterEl.append(imgEl)
+    posterEl.append(infoEl)
+
+    liEl.classList.add('list')
+    titleEl.classList.add('title')
     titleEl.textContent = movie.Title
-    posterEl.src = movie.Poster
+    infoEl.classList.add('info')
+    yearEl.classList.add('year')
+    yearEl.textContent = movie.Year
+    imgEl.classList.add('poster')
+    imgEl.style.backgroundImage = movie.Poster === 'N/A' ? `url(../images/no-image.webp)` : `url(${movie.Poster})`
+    imgEl.style.backgroundPosition = 'center'
 
-    liEl.append(titleEl, posterEl)
+    liEl.append(posterEl)
 
     return liEl
   })
   moviesEl.append(...movieEls)
 }
+window.addEventListener('scroll', function() {
+  if(window.scrollY >= document.documentElement.scrollHeight - window.innerHeight){
+    if(document.querySelector('.movies > li')){
+      viewmore()
+    }
+  }
+})
+
+
+
+// to-top 버튼
+window.addEventListener('scroll', _.throttle(function(){
+  if(window.scrollY > 500){
+    gsap.to(toTopEl, 0, {
+      x: -132
+    });
+  }
+  else{
+    gsap.to(toTopEl, 0, {
+      x: 100
+    });
+  }
+}, 300));
+
+toTopEl.addEventListener('click', function () {
+  gsap.to(window, .7, {
+    scrollTo: 0
+  });
+});
