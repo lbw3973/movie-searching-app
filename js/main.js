604ea34eb54d97100f57b515e1aa1fa240c069b1
LeeByoungWook <lbw3973@naver.com>
2023-01-13 21:34:44 +0900

    feat: All Done!!!
    - 영화 포스터 클릭시 dialog 출력 완료
    - dialog이 show 되었을때, body scroll disable

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+const mainIconEl = document.querySelector('.main-icon')
 const inputEl = document.querySelector('input')
 const buttonEl = document.querySelector('button#btnSearch')
 const moviesEl = document.querySelector('ul.movies')
@@ -5,7 +6,11 @@ const counterEl = document.querySelector('.counter')
 const toTopEl = document.querySelector('#to-top');
 const selectEl = document.querySelector('#select-year')
 const curYear = new Date().getFullYear()
-const detailEl = document.querySelector('.dialog')
+const detailEl = document.querySelector('.detail')
+const dialogEl = detailEl.querySelector('.dialog')
+const dlgImageEl = dialogEl.querySelector('.movie-image')
+const dlgInfoEl = dialogEl.querySelector('.movie-info')
+const btnCloseEl = document.querySelector('#btnClose')
 
 let searchText = ''
 let pageNumber = 1
@@ -21,20 +26,19 @@ for(let i = curYear; i >= curYear - 50; --i){
 
 inputEl.addEventListener("keyup", function (event) {
   if(event.key === 'Enter'){
-    event.preventDefault()
     btnSearch.click()
   }
 })
 
 buttonEl.addEventListener('click', async function () {
+  initialize()
   searchText = inputEl.value
   searchYear = selectEl.value
+
   if(searchText.length < 3){
     alert("Please enter at least 3 characters")
     return
   }
-  pageNumber = 1
-  moviesEl.innerHTML = ''
 
   const movies = await getMovies(searchText, searchYear, pageNumber)
   await getCountMovies(searchText, searchYear)
@@ -73,17 +77,17 @@ const viewmore = async () => {
   const movies = await getMovies(searchText, searchYear, ++pageNumber)
   if(movies != null){
     renderMovies(movies, false)
+    showMovieDialog()
   }
 }
 
 
-async function getMovies(title, year = '', page = 1) {
+const getMovies = async (title, year = '', page = 1) => {
   const s = `&s=${title}`
   const y = year === 'all' ? '' : `&y=${year}`
   const p = `&page=${page}`
   const res = await fetch(`https://omdbapi.com/?apikey=7035c60c&s=${s}${y}${p}`)
   const json = await res.json()
-  console.log(json)
   let moviess = json.Search
 
   if(json.Response === "True"){
@@ -93,7 +97,7 @@ async function getMovies(title, year = '', page = 1) {
   
 }
 
-function renderMovies(movies, isFirst) {
+const renderMovies = (movies, isFirst) => {
   const movieEls = movies.map(function (movie) {
     const liEl = document.createElement('li')
     const posterEl = document.createElement('a')
@@ -134,33 +138,112 @@ window.addEventListener('scroll', function() {
   }
 })
 
-function showMovieDialog() {
+
+const showMovieDialog = () => {
   const movies = moviesEl.querySelectorAll('li')
 
-  movies.forEach(x => x.addEventListener('click', (event) => {
+  movies.forEach(x => x.addEventListener('click', () => {
     const movieId = x.dataset.id
+    
+    // dialog 띄었을 때, Scroll Lock
+    document.getElementsByTagName('body')[0].classList.add('scroll-lock')
+
+    detailEl.style.display = 'block'
+    dialogEl.style.backgroundImage = `url(../images/loading.gif)`
+    dialogEl.style.backgroundPosition = 'center'
+    dialogEl.style.backgroundRepeat = 'no-repeat'
 
     getMovieInfo(movieId)
   }))
 }
 
-async function getMovieInfo(id) {
+const getMovieInfo = async (id) => {
   const res = await fetch(`https://omdbapi.com/?apikey=7035c60c&i=${id}&plot=full`)
   const json = await res.json()
+  console.log(json)
 
   if (json.Response === 'True') {
-    // return json
     setMovieInfo(json)
   }
   return json.Error
 }
 
-function setMovieInfo(movie) {
-  detailEl.style.display = 'block'
-  detailEl.innerHTML = 'asdwqdqwasdasdwqwqdd'
+const setMovieInfo = (movie) => {
+  if(dlgInfoEl.querySelector('li'))
+    return
+
+  
+  const liEl = document.createElement('li')
+  const title = document.createElement('h2')
+  const country = document.createElement('div')
+  const director = document.createElement('div')
+  const actor = document.createElement('div')
+  const metaScore = document.createElement('div')
+  const genre = document.createElement('div')
+
+  title.textContent = movie.Title
+  country.textContent = movie.Country
+  director.textContent = movie.Director
+  actor.textContent = movie.Actors
+  metaScore.textContent = movie.Metascore
+  genre.textContent = movie.Genre
+
+  title.classList.add('title')
+  country.classList.add('country')
+  director.classList.add('director')
+  actor.classList.add('actor')
+  metaScore.classList.add('metascore')
+  genre.classList.add('boxoffice')
+
+  // const img = movie.Poster === 'N/A' ? `url(../images/no-iamge.webp)` : movie.Poster.replace('X300', 'X500')
+  // dlgImageEl.style.backgroundImage = `url(${img})`
+  dlgImageEl.style.backgroundImage = movie.Poster === 'N/A' ? `url(../images/no-image.webp)` : `url(${movie.Poster})`
+  dlgImageEl.style.backgroundPosition = 'center'
+  dlgImageEl.style.backgroundRepeat = 'no-repeat'
+  dialogEl.style.backgroundImage = 'none'
+
+  liEl.append(title)
+  liEl.append(country)
+  liEl.append(director)
+  liEl.append(actor)
+  liEl.append(metaScore)
+  liEl.append(genre)
+
+  dlgInfoEl.append(liEl)
+}
+
+// dialog 닫는 동작 2가지
+detailEl.addEventListener('click', (e) => {
+  if(e.target.className === 'detail'){
+    dlgMovieInfoReset()
+  }
+})
+window.addEventListener('keyup', (e) => {
+  if(e.key === 'Escape'){
+    dlgMovieInfoReset()
+  }
+})
+btnCloseEl.addEventListener('click', () => {
+  dlgMovieInfoReset()
+})
+const dlgMovieInfoReset = () => {
+  detailEl.style.display = 'none'
+  dlgImageEl.style.backgroundImage = ''
+  dlgInfoEl.innerHTML = ''
+  document.getElementsByTagName('body')[0].classList.remove('scroll-lock')
 }
 
 
+// 초기화
+const initialize = () => {
+  pageNumber = 1
+  moviesEl.innerHTML = ''
+  counterEl.innerHTML = ''
+}
+mainIconEl.addEventListener('click', () => {
+  initialize()
+  inputEl.value = ''
+})
 
 // to-top 버튼
 window.addEventListener('scroll', _.throttle(function() {
